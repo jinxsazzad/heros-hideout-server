44d5cab7f531e033a289f008f9d6bc96c5f54b86
jinxsazzad <sazzad3110@gmail.com>
2023-05-21 01:52:49 +0600

    done everythin on server site

--- index.js
+++ index.js
@@ -25,93 +25,120 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const db = client.db("herosHideout");
-    const toysCollection = db.collection("allToy");
-
-    //all post api
-    app.post("/addAToy", async (req, res) => {
-      const body = req.body;
-      const result = await toysCollection.insertOne(body);
-      res.send(result);
-    });
-
-    //all get api
-
-    //1.'/allToy' for load all data from this collection
-    app.get("/allToy", async (req, res) => {
-      const limit = parseInt(req.query.limit) || 20;
-      const allToy = await toysCollection.find({}).limit(limit).toArray();
-      res.send(allToy);
-    });
-
-    //2.'/toyDetails/:id' for load a toy data from this collection by id
-    app.get("/toyDetails/:id", async (req, res) => {
-      const toy = await toysCollection.findOne({
-        _id: new ObjectId(req.params.id),
-      });
-      res.send(toy);
-    });
-
-    //3.'/myToys/:email' for load all data from this collection by a user
-    app.get("/myToys/:email", async (req, res) => {
-      const myToys = await toysCollection
-        .find({
-          sellerMail: req.params.email,
-        })
-        .toArray();
-      res.send(myToys);
-    });
-
-    //4.'/toysBySubCategory/:subCat' for load all data from this collection by a sub-category
-    app.get("/toysBySubCategory/:subCat", async (req, res) => {
-      const subCategoryToys = await toysCollection
-        .find({
-          subCategory: req.params.subCat,
-        })
-        .toArray();
-      res.send(subCategoryToys);
-    });
-
-    // all put and fetch api
-
-    //1.'/toyDetails/:id' for edit a toy data from this collection by id
-    app.put("/toyDetails/:id", async (req, res) => {
-      const id = req.params.id;
-      const updateToy = req.body;
-      const filter = { _id: new ObjectId(id) };
-      const updateDoc = {
-        $set: {
-          description: updateToy.description,
-          name: updateToy.name,
-          pictureUrl: updateToy.pictureUrl,
-          price: updateToy.price,
-          quantity: updateToy.quantity,
-          rating: updateToy.rating,
-          sellerMail: updateToy.sellerMail,
-          sellerName: updateToy.sellerName,
-          subCategory: updateToy.subCategory,
-        },
-      };
-      const result = await toysCollection.updateOne(filter, updateDoc);
-      res.send(result);
-    });
-    // all delete api
-    app.delete("/toyDetails/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await toysCollection.deleteOne(query);
-      res.send(result);
-    });
-
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("MongoDB Connected !");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("MongoDB Connected !");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
+
+  // avoid try and finally
+  // Connect the client to the server	(optional starting in v4.7)
+  await client.connect();
+  const db = client.db("herosHideout");
+  const toysCollection = db.collection("allToy");
+
+  //all get api
+
+  // for load all data from this collection
+  app.get("/allToy", async (req, res) => {
+    const limit = parseInt(req.query?.limit);
+    const allToy = await toysCollection.find({}).limit(limit).toArray();
+    res.send(allToy);
+  });
+
+  // for search
+  app.get("/findToyByName/:name", async (req, res) => {
+    const name = req.params.name;
+
+    const query = { name: { $regex: name, $options: "i" } };
+    const result = await toysCollection.find(query).toArray();
+
+    res.send(result);
+  });
+
+  // for load a toy data from this collection by id
+  app.get("/toyDetails/:id", async (req, res) => {
+    const toy = await toysCollection.findOne({
+      _id: new ObjectId(req.params.id),
+    });
+    res.send(toy);
+  });
+
+  // for load all data from this collection by a user
+  app.get("/myToys/:email", async (req, res) => {
+    const sort = req.query?.sort;
+    if (sort == true) {
+      const myToys = await toysCollection
+        .find({
+          sellerMail: req.params.email,
+        })
+        .sort({ price: 1 })
+        .collation({ locale: "en_US", numericOrdering: true })
+        .toArray();
+      res.send(myToys);
+    } else {
+      const myToys = await toysCollection
+        .find({
+          sellerMail: req.params.email,
+        })
+        .collation({ locale: "en_US", numericOrdering: true })
+        .toArray();
+      res.send(myToys);
+    }
+  });
+
+  // for load all data from this collection by a sub-category
+  app.get("/toysBySubCategory/:subCat", async (req, res) => {
+    const subCategoryToys = await toysCollection
+      .find({
+        subCategory: req.params.subCat,
+      })
+      .toArray();
+    res.send(subCategoryToys);
+  });
+
+  //all post api
+  // for add a toy
+  app.post("/addAToy", async (req, res) => {
+    const body = req.body;
+    const result = await toysCollection.insertOne(body);
+    res.send(result);
+  });
+
+  // all put and fetch api
+
+  // for edit a toy data from this collection by id
+  app.put("/toyDetails/:id", async (req, res) => {
+    const id = req.params.id;
+    const updateToy = req.body;
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        description: updateToy.description,
+        name: updateToy.name,
+        pictureUrl: updateToy.pictureUrl,
+        price: updateToy.price,
+        quantity: updateToy.quantity,
+        rating: updateToy.rating,
+        sellerMail: updateToy.sellerMail,
+        sellerName: updateToy.sellerName,
+        subCategory: updateToy.subCategory,
+      },
+    };
+    const result = await toysCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  });
+
+  // all delete api
+  // for delete a toy by id
+  app.delete("/toyDetails/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await toysCollection.deleteOne(query);
+    res.send(result);
+  });
 }
 run().catch(console.dir);
 
